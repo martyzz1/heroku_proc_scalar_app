daf640bddcbca3ebd93be0f32edc09f676a4f6fc
Martyzz1 <martyzz1@gmail.com>
2012-10-09 10:08:24 +0100

    Some tweaks

--- worker.py
+++ worker.py
@@ -14,6 +14,8 @@ SLEEP_PERIOD = float(os.environ.get('SLEEP_PERIOD', 10))
 COUNT_BOUNDARY = os.environ.get('COUNT_BOUNDARY', 0)
 HEROKU_API_KEY = os.environ.get('HEROKU_API_KEY', False)
 
+max_str_length = 90
+
 assert(DATABASE_URL)
 assert(HEROKU_API_KEY)
 
@@ -33,7 +35,7 @@ def _get_database():
 def process_apps(app, heroku_conn):
 
     data = get_data(app)
-    pprint(data)
+    #pprint(data)
     if not data:
         return
 
@@ -43,11 +45,10 @@ def process_apps(app, heroku_conn):
         try:
             heroku_app = heroku_conn.apps[app.appname]
         except KeyError:
-            print "[ERROR] %s is not available via your configured HEROKU_API %s" % (app.appname, HEROKU_API_KEY)
+            print "\n[ERROR] %s is not available via your configured HEROKU_API %s.\nAvailable apps are:-\n" % (app.appname, HEROKU_API_KEY)
             pprint(heroku_conn.apps)
-            pprint(heroku_conn.apps[app.appname])
         else:
-            print "\n\n[%s] Checking for scaling on %s" % (app.appname, procname)
+            print "\r[%s] Checking for scaling on %s".ljust(max_str_length) % (app.appname, procname),
             check_for_scaling(heroku_conn, heroku_app, app, procname, count, active_count)
 
 
@@ -71,29 +72,22 @@ def shutdown_app(heroku_conn, app, procname):
     heroku_app = heroku_conn.apps[app.appname]
     running_already = 0
     cmd = "fab shutdown_celery_process:%s" % procname
-    pprint(heroku_app.processes)
+    #pprint(heroku_app.processes)
     try:
         web_proc = heroku_app.processes['run']
     except KeyError:
         running_already = 0
     else:
-        print "possibly got running process, checking for %s" % cmd
+        print "\rpossibly got running process, checking for %s".ljust(max_str_length) % cmd,
         for proc in web_proc:
             if proc.command == cmd:
                 running_already = 1
 
     if running_already == 1:
-        print "[%s] Shutdown command already running... skipping...."
+        print "\r[%s] Shutdown command for %s already running... skipping....".ljust(max_str_length) % (app.appname, procname)
     else:
-        print "[%s] shutting down processes %s" % (app.appname, procname)
+        print "\r[%s] shutting down processes %s".ljust(max_str_length) % (app.appname, procname)
         heroku_app.run_async(cmd)
-    #is it already running?
-    #if get_current_dynos(heroku_app, control_app):
-        #print "[%s]  WARN - control_app is already in the process of shutting down processes, doing nothing" % app.appname
-    #else:
-        #start the control_app
-        #print "[%s] starting control_app %s to shutdown processes" % (app.appname, control_app)
-        #scale_dyno(heroku_conn, heroku_app, app, control_app, 1)
 
 
 def get_current_dynos(heroku_app, procname):
@@ -105,7 +99,7 @@ def get_current_dynos(heroku_app, procname):
 
         cpt = 0
         for proc in web_proc:
-            pprint(proc.state)
+            #pprint(proc.state)
             cpt += 1
 
         return cpt
@@ -117,13 +111,12 @@ def check_for_scaling(heroku_conn, heroku_app, app, procname, count, active_task
     required_count = calculate_required_dynos(count)
     current_dyno_count = int(get_current_dynos(heroku_app, procname))
 
-    print "[%s] current task count for %s = %s" % (appname, procname, count)
-    print "[%s] current_dyno_count for %s = %s" % (appname, procname, current_dyno_count)
+    print "\r[%s] %s has %s running dynos and %s pending tasks.".ljust(max_str_length) % (appname, procname, current_dyno_count, count),
 
     if not current_dyno_count == required_count:
-        print "[%s] Scaling dyno process %s to %d" % (appname, procname, required_count)
+        print "\r[%s] Scaling %s dyno process to %d".ljust(max_str_length) % (appname, procname, required_count),
         if required_count == 0 and active_tasks > 0:
-            print "[%s] Not Scaling %s dyno to 0 yet as it still has %s active tasks" % (appname, procname, active_tasks)
+            print "\r[%s] Not shutting down %s dyno yet as it still has %s active tasks".ljust(max_str_length) % (appname, procname, active_tasks),
         else:
             scale_dyno(heroku_conn, heroku_app, app, procname, required_count)
 
@@ -146,12 +139,14 @@ def get_data(app):
 
     r = ''
     if app.username or app.password:
+        print "\r[%s]Loading data, using authentication method please wait.....".ljust(max_str_length) % app.appname,
         r = requests.get(app.app_api_url, auth=(app.user, app.password))
         if not r.status_code == 200:
-            print "[ERROR] %s call to %s with user = %s and password = %s Returned response code %s and the following message" % (app.appname, app.app_api_url, app.username, app.password, r.status_code)
+            print "\n[ERROR] %s call to %s with user = %s and password = %s Returned response code %s and the following message" % (app.appname, app.app_api_url, app.username, app.password, r.status_code)
             print r.text
             return
     else:
+        print "\r[%s]Loading data, please wait.....".ljust(max_str_length) % app.appname,
         r = requests.get(app.app_api_url)
         if not r.status_code == 200:
             print "[ERROR] %s call to %s without user or password Returned response code %s and the following message" % (app.appname, app.app_api_url, r.status_code)
@@ -164,11 +159,11 @@ def get_data(app):
 engine = _get_database()
 Session = scoped_session(sessionmaker(bind=engine))
 while(True):
-    print "==============================================================================="
+    print "\rBeginning Run............. ".ljust(max_str_length),
     session = Session()
     apps = session.query(App).all()
     heroku_conn = heroku.from_key(HEROKU_API_KEY)
     for app in apps:
         process_apps(app, heroku_conn)
-    print "[INFO] sleeping for %f" % SLEEP_PERIOD
+    print "\rCycle Complete sleeping for %f".ljust(max_str_length) % SLEEP_PERIOD,
     time.sleep(SLEEP_PERIOD)
