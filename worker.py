60c86d1f71c97e310e427c9e27c8cddde76fe5b3
Martyzz1 <martyzz1@gmail.com>
2012-10-12 13:36:06 +0100

    Minor tweak

--- worker.py
+++ worker.py
@@ -146,7 +146,7 @@ def get_data(app):
             print r.text
             return
     else:
-        #print "\r[%s]Loading data, please wait.....".ljust(max_str_length) % app.appname,
+        print "\r[%s]Loading data, please wait.....".ljust(max_str_length) % app.appname,
         r = requests.get(app.app_api_url)
         if not r.status_code == 200:
             print "[ERROR] %s call to %s without user or password Returned response code %s and the following message" % (app.appname, app.app_api_url, r.status_code)
