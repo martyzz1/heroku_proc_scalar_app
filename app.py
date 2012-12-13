c09697c6dbdbbafeb30b3a689f67c5cbe0a4594f
Martyzz1 <martyzz1@gmail.com>
2012-12-13 17:37:58 +0000

    Change column type

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer
 
 Base = declarative_base()
 
@@ -11,6 +11,6 @@ class App(Base):
     app_api_url = Column('app_api_url', String(250))
     username = Column('username', String(50), nullable=True)
     password = Column('password', String(50), nullable=True)
-    min_dynos = Column('min_dynos', String(5), nullable=True)
-    max_dynos = Column('max_dynos', String(5), nullable=True)
-    count_boundary = Column('count_boundary', String(1), nullable=True)
+    min_dynos = Column('min_dynos', Integer(5), nullable=True)
+    max_dynos = Column('max_dynos', Integer(5), nullable=True)
+    count_boundary = Column('count_boundary', Integer(1), nullable=True)
