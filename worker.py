eb0e2b14da44c9c9e1efebdf991fbd57d72859bf
Robin Edwards <robin.ge@gmail.com>
2013-06-03 14:52:38 +0100

    Check key exists first

--- worker.py
+++ worker.py
@@ -41,9 +41,8 @@ def process_apps(app, heroku_conn):
     for procname in data.iterkeys():
         count = data[procname]['count']
         active_count = data[procname]['active']
-        deployment_locked = data[procname]['deploy_lock']
-        if not deployment_locked == 0:
-            print "[%s] %s is locked for %s, skipping".ljust(max_str_length) % (app.appname, procname, deployment_locked)
+        if 'deploy_lock' in data[procname] and data[procname]['deploy_lock'] != 0:
+            print "[%s] %s is locked for %s, skipping".ljust(max_str_length) % (app.appname, procname, data[procname]['deploy_lock'])
             continue
         try:
             heroku_app = heroku_conn.apps[app.appname]
